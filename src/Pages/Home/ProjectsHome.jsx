c7c00452d198c1eb81ae8f8b0357f66d9a30d083
EyxOfc <eyx.ofc@gmail.com>
2024-03-24 21:58:03 -0300

    Up

--- src/Pages/Home/ProjectsHome.jsx
+++ src/Pages/Home/ProjectsHome.jsx
@@ -6,7 +6,7 @@ function ProjectsHome() {
         <>
         <section id='ProjectsHome' className='flex flex-row flex-wrap items-center justify-center sm:gap-[40px] gap-[20px] w-full p-[20px] '>
 
-            <Projeto Title='Nome do Projeto' Text='Descrição do Projeto' To=''/>
+            <Projeto Title='Security Tech' Text='Sistema onde pode colocar suas senhas, Sistema de Auth e CRUD, Em breve no ar.' To='https://securitytech.netlify.app'/>
 
             <Projeto Title='Nome do Projeto' Text='Descrição do Projeto' To=''/>
 
