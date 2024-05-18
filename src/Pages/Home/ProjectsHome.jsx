f8fc82b5c14018f0384c040ccf3085a432b0a1f9
EyxOfc <eyx.ofc@gmail.com>
2024-05-18 19:41:45 -0300

    Up

--- src/Pages/Home/ProjectsHome.jsx
+++ src/Pages/Home/ProjectsHome.jsx
@@ -6,8 +6,6 @@ function ProjectsHome() {
         <>
         <section id='ProjectsHome' className='flex flex-row flex-wrap items-center justify-center sm:gap-[40px] gap-[20px] w-full p-[20px] '>
 
-            <Projeto Title='Security Tech' Text='Sistema onde pode colocar suas senhas, Sistema de Auth e CRUD, Em breve no ar.' To='https://securitytech.netlify.app'/>
-
             <Projeto Title='Paxio' Text='Pacote npm que gera uma senha, Com 4 a 8 caracteres.' To='https://paxio.netlify.app'/>
 
             <Projeto Title='Nome do Projeto' Text='Descrição do Projeto' To=''/>
